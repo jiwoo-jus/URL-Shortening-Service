1f7622702ed81c5d8c3bec5d6283e3f146a2c5aa
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-12-13 23:30:18 +0900

    @NotBlank 어노테이션 제거 #3
    이유: @Pattern(url 정규식) 하나로 빈 값, 공백 체크 가능하므로 중첩해 사용할 필요 없음.

--- src/main/java/com/jiwjus/urlshorteningservice/dto/UrlRequestDto.java
+++ src/main/java/com/jiwjus/urlshorteningservice/dto/UrlRequestDto.java
@@ -1,13 +1,14 @@
 package com.jiwjus.urlshorteningservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+@AllArgsConstructor
 @Getter
 public class UrlRequestDto {
-    @NotBlank(message = "url을 입력하세요.")
+
     @Pattern(message = "올바른 url 형식이 아닙니다.", regexp="^((http(s?))\\:\\/\\/)([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]{2,6}(\\:[0-9]+)?(\\/\\S*)?$")
     private String originalUrl;
 }
