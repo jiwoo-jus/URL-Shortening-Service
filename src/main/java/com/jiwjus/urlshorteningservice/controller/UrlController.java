9ee316003ab17c9c7ffccdba32b33419450237cc
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-12-13 15:32:16 +0900

    입력 url 유효성 검사 및 에러메세지 첨부 #3
    UrlRequestDto - originalUrl
    1. @NotBlank validation 적용
    2. @Pattern(정상 url 정규식) validation 적용

--- src/main/java/com/jiwjus/urlshorteningservice/controller/UrlController.java
+++ src/main/java/com/jiwjus/urlshorteningservice/controller/UrlController.java
@@ -5,11 +5,15 @@ import com.jiwjus.urlshorteningservice.dto.UrlResponseDto;
 import com.jiwjus.urlshorteningservice.service.UrlShorteningService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,7 +23,11 @@ public class UrlController {
     private final UrlShorteningService urlShorteningService;
 
     @PostMapping
-    public ResponseEntity<?> shortenUrl(@RequestBody UrlRequestDto urlRequestDto){
+    public ResponseEntity<?> shortenUrl(@Valid @RequestBody UrlRequestDto urlRequestDto, Errors errors){
+        if (errors.hasErrors()){
+            String errMsg = Objects.requireNonNull(errors.getFieldError()).getDefaultMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errMsg);
+        }
         UrlResponseDto urlResponseDto = urlShorteningService.shortenUrl(urlRequestDto.getOriginalUrl());
         return ResponseEntity.ok(urlResponseDto);
     }
--- src/main/java/com/jiwjus/urlshorteningservice/dto/UrlRequestDto.java
+++ src/main/java/com/jiwjus/urlshorteningservice/dto/UrlRequestDto.java
@@ -1,13 +1,13 @@
 package com.jiwjus.urlshorteningservice.dto;
 
 import lombok.Getter;
-import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 public class UrlRequestDto {
-    @URL
-    @NotBlank
+    @NotBlank(message = "url을 입력하세요.")
+    @Pattern(message = "올바른 url 형식이 아닙니다.", regexp="^((http(s?))\\:\\/\\/)([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]{2,6}(\\:[0-9]+)?(\\/\\S*)?$")
     private String originalUrl;
 }
