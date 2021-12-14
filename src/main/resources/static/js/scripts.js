8d18ffab817008fa62aa4a2d78c9eff1e53bf130
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-12-14 23:02:00 +0900

    사용자 화면 - 화면 구성 완료 #4
    1. original url 입력폼
    2. shorted url 출력
    3. number of requests 출력

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -3,5 +3,41 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+window.addEventListener("load", function () {
+    document.getElementById('contactForm').addEventListener("submit", function (e) {
+        e.preventDefault(); // before the code
+        /* do what you want with the form */
+        const longUrl = e.target.elements[0].value
+
+        // send api request
+        // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        // fetch('http://example.com/movies.json')
+        // .then(response => response.json())
+        // .then(data => console.log(data));
+
+        handleResponse('http://localhost:8088/b', 1)
+
+        // if error -> put this in catch
+        // handleResponseError()
+    })
+});
+
+
+const handleResponse = (shortUrl, requestCount) => {
+    const requestCountElement = document.getElementById('requestCount');
+    const shortUrlElement = document.getElementById('shortUrl');
+    requestCountElement.innerText = `Request Number: ${requestCount}`
+    shortUrlElement.innerText = `Shortened URL: ${shortUrl}`
+    copyToClipboard(shortUrl)
+}
+
+const handleResponseError = () => {
+    const errorElement = document.getElementById('submitErrorMessage')
+    errorElement.innerText = 'Error sending message'
+}
+
+function copyToClipboard(text) {
+    if (navigator && 'clipboard' in navigator) {
+        navigator.clipboard.writeText(text);
+    }
+}
